572df32d9f77dcde5a290007c82aff4ee6235f69
BANCOARBI\alexandre.arphoenix <alexandre.arphoenix@bancoarbi.com.br>
2021-08-26 16:44:32 -0300

    feature para tornar o dicionário dinamino com tratamento de caractes.

--- automa.py
+++ automa.py
@@ -4,62 +4,53 @@ from openpyxl.descriptors import MinMax, Sequence
 import time
 import sys
 from tkinter import filedialog
+import unicodedata
 
-SelecArquivos = filedialog.askopenfilenames()
-arquivos = ["C:/Users/alexandre.borges/Documents/TCU/pubs_secao3_2020-10_(outubro).xlsx"]
-
-for item in SelecArquivos:
-  print(item)
-  wb = load_workbook(item)
-  ws = wb.active
-  col = ws['F']
-  linha = 2
-
-  dicionario = [
-      "Banco do Brasil",
-      "Banco do Nordeste",
-      "Banco da Amazônia",
-      "Caixa Econômica Federal",
-      "Banco Central",
-      "Casa da Moeda",
-      "Comissão de Valores Mobiliários",
-      "Susep",
-      "BNDES",
-      "Associação Brasileira de Fundos Garantidores",
-      "Banco Nacional de Desenvolvimento Econômico e Social"
-  ]
-
-
-  def filtro(dicionario, max_row, linha):
-      while linha <= ws.max_row:
-          for item in dicionario:
-              try:
-                  if item in ws['F' + str(linha)].value:
-                      print(ws['F' + str(linha)].value)
-                      break
-                  else:
-                      if item == dicionario[-1]:
-                          ws.delete_rows(linha)
-                      else:
-                          continue
-              except:
-                  ws.delete_rows(linha)
-          linha = linha + 1
-
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-  filtro(dicionario, ws.max_row, linha)
-
-  '''ultima = ws.max_row
-  while ws['F' + str(ultima)].value != "":
-    filtro(dicionario, ws.max_row, linha)'''
+dicionario = []
+def lista(linha,dicionario):
+    SelecArquivos = filedialog.askopenfilename(title='Dicionário de dados')
+    wb = load_workbook(SelecArquivos)
+    ws = wb.active
+    while linha <= ws.max_row:
+        dicionario.append(unicodedata.normalize('NFKC',ws['A' + str(linha)].value))
+        linha += 1
+    return dicionario
     
+def planingauto(dicionario):
+    SelecArquivos = filedialog.askopenfilenames(title='Planilha a ser filtrada')
+
+    for item in SelecArquivos:
+        print(item)
+        wb = load_workbook(item)
+        ws = wb.active
+        col = ws['F']
+        linha = 2
     
-  wb.save(item)
+    def filtro(dicionario,linha):
+        while linha <= ws.max_row:
+            for item in dicionario:
+                try:
+                    if item in ws['F' + str(linha)].value:
+                        print(ws['F' + str(linha)].value)
+                        break
+                    else:
+                        if item == dicionario[-1]:
+                            ws.delete_rows(linha)
+                        else:
+                            continue
+                except:
+                    ws.delete_rows(linha)
+            linha += 1
+
+    filtro(dicionario,linha)
+    filtro(dicionario, linha)
+    filtro(dicionario, linha)
+    filtro(dicionario, linha)
+    filtro(dicionario,linha)
+    filtro(dicionario, linha)
+    filtro(dicionario, linha)
+    filtro(dicionario, linha)            
+    wb.save(item)
+
+lista(1,dicionario)
+planingauto(dicionario)
